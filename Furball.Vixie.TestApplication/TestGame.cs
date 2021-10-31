6963f1672c908a7c879c35301a34653a36e75df2
Eevee <developerbrzoza@gmail.com>
2021-10-31 14:45:10 +0100

    Matricies: alot of matrix stuff but the fucking y axis is fucked

--- Furball.Vixie.TestApplication/TestGame.cs
+++ Furball.Vixie.TestApplication/TestGame.cs
@@ -25,10 +25,11 @@ namespace Furball.Vixie.TestApplication {
 
         protected override unsafe void Initialize() {
             float[] verticies = new float[] {
-                /* Vertex Coordinates */ -0.5f, -0.5f, /* Texture Coordinates */ 0.0f, 0.0f,
-                /* Vertex Coordinates */  0.5f, -0.5f, /* Texture Coordinates */ 1.0f, 0.0f,
-                /* Vertex Coordinates */  0.5f,  0.5f, /* Texture Coordinates */ 1.0f, 1.0f,
-                /* Vertex Coordinates */ -0.5f,  0.5f, /* Texture Coordinates */ 0.0f, 1.0f,
+
+                /* Vertex Coordinates */  100f,  (100f), /* Texture Coordinates */ 0.0f, 0.0f, //Bottom Left corner
+                /* Vertex Coordinates */  200f,  (100f), /* Texture Coordinates */ 1.0f, 0.0f, //Bottom Right corner
+                /* Vertex Coordinates */  200f,  (200f), /* Texture Coordinates */ 1.0f, 1.0f, //Top Right Corner
+                /* Vertex Coordinates */  100f,  (200f), /* Texture Coordinates */ 0.0f, 1.0f, //Top Left Corner
             };
 
             //Indicies, basically what order to draw both triangles in
@@ -36,12 +37,11 @@ namespace Furball.Vixie.TestApplication {
             //so the first triangle will take index 0 1 and 2 from vertecies
             //and the 2nd triangle will take index 2 3 and 0, which together forms a quad
             uint[] indicies = new uint[] {
+                //Triangle 1 from bottom left, to bottom right, to top right corner
                 0, 1, 2,
+                //Triangle 2 from top right, to top left, to bottom left corner
                 2, 3, 0
             };
-            //load shader sources
-            string vertexSource = ResourceHelpers.GetStringResource("Shaders/BasicTexturedVertexShader.glsl", true);
-            string fragmentSource = ResourceHelpers.GetStringResource("Shaders/BasicTexturedPixelShader.glsl", true);
 
             //prepare buffers
             this._vertexBuffer      = new BufferObject<float>(verticies, BufferTargetARB.ArrayBuffer);
@@ -57,11 +57,21 @@ namespace Furball.Vixie.TestApplication {
             //describe layout of VAO
             this._vertexArrayObject.AddBuffer(this._vertexBuffer, layout);
 
-            Matrix4x4 projectionMatrix = Matrix4x4.CreateOrthographic(-8f, 8f, -4.5f, 4.5f);
+            //Used for putting everything into a Coordinate space that we can work with
+            Matrix4x4 projectionMatrix = Matrix4x4.CreateOrthographicOffCenter(0, 1280, 0, 720, 0f, 1f);;
+
+
+            //Used for simulating a camera
+            Matrix4x4 viewMatrix = Matrix4x4.CreateTranslation(new Vector3(0, 0, 0));
+            //Used for putting the model/object into a certain position
+            Matrix4x4 modelMatrix = Matrix4x4.CreateTranslation(new Vector3(0, 0.5f, 0));
+
+            Matrix4x4 mvp = projectionMatrix * viewMatrix * modelMatrix;
 
             //Create and initialize shader
             this._shader = new BasicTexturedShader();
-            this._shader.SetUniform("u_ProjectionMatrix", UniformType.GlMat4f, projectionMatrix);
+            this._shader.SetUniform("u_ProjectionMatrix", UniformType.GlMat4f, mvp);
+            //this._shader.SetUniform("u_WindowSize", UniformType.GlFloat, 1280f, 720f);
 
             this._texture = new Texture(ResourceHelpers.GetByteResource("Resources.pippidonclear0.png"));
             this._texture.Bind();
