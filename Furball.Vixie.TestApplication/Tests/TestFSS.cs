7f4847ff2e7c1c14ab23c0c0ce3eed4c7ecf3626
Eevee <developerbrzoza@gmail.com>
2021-11-06 02:55:59 +0100

    TestFSS: ImGui stuffs

--- Furball.Vixie.TestApplication/Tests/TestFSS.cs
+++ Furball.Vixie.TestApplication/Tests/TestFSS.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Drawing;
+using System.Globalization;
 using System.IO;
 using System.Numerics;
 using FontStashSharp;
 using Furball.Vixie.FontStashSharp;
 using Furball.Vixie.Graphics.Renderers.OpenGL;
+using Furball.Vixie.ImGuiHelpers;
+using ImGuiNET;
+using Silk.NET.OpenGL.Extensions.ImGui;
 
 namespace Furball.Vixie.TestApplication.Tests {
     public class TestFSS : GameComponent {
@@ -11,6 +16,7 @@ namespace Furball.Vixie.TestApplication.Tests {
         private VixieFontStashRenderer _renderer;
         private BatchedRenderer        _batchedRenderer;
         private DynamicSpriteFont      _font;
+        private ImGuiController        _imGuiController;
 
         public override void Initialize() {
             this._system = new FontSystem();
@@ -19,6 +25,7 @@ namespace Furball.Vixie.TestApplication.Tests {
 
             this._batchedRenderer = new BatchedRenderer();
             this._renderer        = new VixieFontStashRenderer(this._batchedRenderer);
+            this._imGuiController      = ImGuiCreator.CreateController();
 
             base.Initialize();
         }
@@ -29,6 +36,26 @@ namespace Furball.Vixie.TestApplication.Tests {
             this._batchedRenderer.Begin();
             this._font.DrawText(this._renderer, "i am in severe pain please make text rendering work", new Vector2(100, 100), Color.White);
             this._batchedRenderer.End();
+
+
+
+            #region ImGui menu
+
+            this._imGuiController.Update((float) deltaTime);
+
+            ImGui.Text($"Frametime: {Math.Round(1000.0f / ImGui.GetIO().Framerate, 2).ToString(CultureInfo.InvariantCulture)} " +
+                       $"Framerate: {Math.Round(ImGui.GetIO().Framerate,           2).ToString(CultureInfo.InvariantCulture)}"
+            );
+
+            if (ImGui.Button("Go back to test selector")) {
+                this.BaseGame.Components.Add(new BaseTestSelector());
+                this.BaseGame.Components.Remove(this);
+            }
+
+            this._imGuiController.Render();
+
+            #endregion
+
 
             base.Draw(deltaTime);
         }
