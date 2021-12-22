f0a7d97d77a9fcfcaa4d97a012b9a76d20016d48
Beyley Thomas <ep1cm1n10n123@gmail.com>
2021-12-22 06:22:43 -0800

    Color: Only 1% of people can reach pink color!

--- Furball.Vixie/Graphics/Color.cs
+++ Furball.Vixie/Graphics/Color.cs
@@ -153,6 +153,7 @@ namespace Furball.Vixie.Graphics {
         public static readonly Color Orange               = new(255, 165, 0);
         public static readonly Color OrangeRed            = new(255, 69, 0);
         public static readonly Color Red                  = new(255, 0, 0);
+        public static readonly Color Pink                 = new(255, 192, 203);
         // ReSharper restore InconsistentNaming
 
         // ReSharper disable CompareOfFloatsByEqualityOperator
