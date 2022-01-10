53aaf70969c6b1b7431179844abd665865c9d714
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-01-09 16:20:16 -0800

    WindowManager: Add ability to alter the output render position globally You can flip squish and squash the screen at will now

--- Furball.Vixie/ShaderCode/BasicTexturedShader/VertexShader.glsl
+++ Furball.Vixie/ShaderCode/BasicTexturedShader/VertexShader.glsl
@@ -7,9 +7,15 @@ out vec2 v_TexCoord;
 
 uniform mat4 vx_WindowProjectionMatrix;
 uniform mat4 u_Translation;
+uniform float u_ModifierX;
+uniform float u_ModifierY;
 
 void main() {
     gl_Position = vx_WindowProjectionMatrix * u_Translation * position;
 
     v_TexCoord = texCoord;
+
+    // This flips the position into the coordinate space we want
+    gl_Position.x *= u_ModifierX;
+    gl_Position.y *= u_ModifierY;
 }
