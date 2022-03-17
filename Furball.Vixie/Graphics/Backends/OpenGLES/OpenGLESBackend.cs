b8f549bed74b7531e1880474b61f0198838c62ed
Eevee <developerbrzoza@gmail.com>
2022-03-17 19:10:35 +0100

    Added a copious amount of Commenting to the Backends

--- Furball.Vixie/Graphics/Backends/OpenGLES/OpenGLESBackend.cs
+++ Furball.Vixie/Graphics/Backends/OpenGLES/OpenGLESBackend.cs
@@ -15,31 +15,47 @@ using Silk.NET.Windowing;
 namespace Furball.Vixie.Graphics.Backends.OpenGLES {
     // ReSharper disable once InconsistentNaming
     public class OpenGLESBackend : GraphicsBackend {
+        /// <summary>
+        /// OpenGLES API
+        /// </summary>
         // ReSharper disable once InconsistentNaming
         private GL gl;
-
+        /// <summary>
+        /// Projection Matrix used to go from Window Coordinates to OpenGLES Coordinates
+        /// </summary>
         internal Matrix4x4 ProjectionMatrix;
-
+        /// <summary>
+        /// Cache for the Maximum amount of Texture units allowed by the device
+        /// </summary>
         private int _maxTextureUnits = -1;
-
         /// <summary>
         /// ImGui Controller
         /// </summary>
-        internal ImGuiController _imGuiController;
-        
-        private static Thread _MainThread;
-
+        internal ImGuiController ImGuiController;
+        /// <summary>
+        /// Stores the Main Thread that OpenGLES commands run on, used to ensure that OpenGLES commands don't run on different threads
+        /// </summary>
+        private static Thread _mainThread;
+        /// <summary>
+        /// Gets the Thread of Operation
+        /// </summary>
         [Conditional("DEBUG")]
         private void GetMainThread() {
-            _MainThread = Thread.CurrentThread;
+            _mainThread = Thread.CurrentThread;
         }
-
+        /// <summary>
+        /// Ensures that OpenGLES commands don't run on the wrong thread
+        /// </summary>
+        /// <exception cref="ThreadStateException">Throws if a cross-thread operation has occured</exception>
         [Conditional("DEBUG")]
         internal void CheckThread() {
-            if (Thread.CurrentThread != _MainThread)
+            if (Thread.CurrentThread != _mainThread)
                 throw new ThreadStateException("You are calling GL on the wrong thread!");
         }
-        
+        /// <summary>
+        /// Used to Initialize the Backend
+        /// </summary>
+        /// <param name="window"></param>
         public override void Initialize(IWindow window) {
             this.GetMainThread();
 
@@ -58,9 +74,11 @@ namespace Furball.Vixie.Graphics.Backends.OpenGLES {
             this.gl.Enable(EnableCap.Blend);
             this.gl.BlendFunc(GLEnum.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
 
-            this._imGuiController = new ImGuiController(gl, Global.GameInstance.WindowManager.GameWindow, Global.GameInstance._inputContext);
+            this.ImGuiController = new ImGuiController(gl, Global.GameInstance.WindowManager.GameWindow, Global.GameInstance._inputContext);
         }
-        
+        /// <summary>
+        /// Checks for OpenGL errors
+        /// </summary>
         [Conditional("DEBUG")]
         public void CheckError() {
             GLEnum error = this.gl.GetError();
@@ -73,29 +91,48 @@ namespace Furball.Vixie.Graphics.Backends.OpenGLES {
 #endif
             }
         }
-
+        /// <summary>
+        /// Used to Cleanup the Backend
+        /// </summary>
         public override void Cleanup() {
             this.gl.Dispose();
         }
-
+        /// <summary>
+        /// Used to Handle the Window size Changing
+        /// </summary>
+        /// <param name="width">New width</param>
+        /// <param name="height">New height</param>
         public override void HandleWindowSizeChange(int width, int height) {
             this.gl.Viewport(0, 0, (uint) width, (uint) height);
 
             this.ProjectionMatrix = Matrix4x4.CreateOrthographicOffCenter(0, width, 0, height, 1f, 0f);
         }
-
+        /// <summary>
+        /// Used to handle the Framebuffer Resizing
+        /// </summary>
+        /// <param name="width">New width</param>
+        /// <param name="height">New height</param>
         public override void HandleFramebufferResize(int width, int height) {
             this.gl.Viewport(0, 0, (uint) width, (uint) height);
         }
-
+        /// <summary>
+        /// Used to Create a Texture Renderer
+        /// </summary>
+        /// <returns>A Texture Renderer</returns>
         public override IQuadRenderer CreateTextureRenderer() {
             return new QuadRendererGL(this);
         }
-
+        /// <summary>
+        /// Used to Create a Line Renderer
+        /// </summary>
+        /// <returns></returns>
         public override ILineRenderer CreateLineRenderer() {
             return new LineRendererGL(this);
         }
-
+        /// <summary>
+        /// Gets the Amount of Texture Units available for use
+        /// </summary>
+        /// <returns>Amount of Texture Units supported</returns>
         public override int QueryMaxTextureUnits() {
             if (this._maxTextureUnits == -1) {
                 this.gl.GetInteger(GetPName.MaxTextureImageUnits, out int maxTexSlots);
@@ -104,43 +141,81 @@ namespace Furball.Vixie.Graphics.Backends.OpenGLES {
 
             return this._maxTextureUnits;
         }
-
+        /// <summary>
+        /// Clears the Screen
+        /// </summary>
         public override void Clear() {
             this.gl.Clear(ClearBufferMask.ColorBufferBit);
         }
-
+        /// <summary>
+        /// Used to Create a TextureRenderTarget
+        /// </summary>
+        /// <param name="width">Width of the Target</param>
+        /// <param name="height">Height of the Target</param>
+        /// <returns></returns>
         public override TextureRenderTarget CreateRenderTarget(uint width, uint height) {
             return new TextureRenderTargetGL(this, width, height);
         }
-
+        /// <summary>
+        /// Creates a Texture given some Data
+        /// </summary>
+        /// <param name="imageData">Image Data</param>
+        /// <param name="qoi">Is the Data in the QOI format?</param>
+        /// <returns>Texture</returns>
         public override Texture CreateTexture(byte[] imageData, bool qoi = false) {
             return new TextureGL(this, imageData, qoi);
         }
-
+        /// <summary>
+        /// Creates a Texture given a Stream
+        /// </summary>
+        /// <param name="stream">Stream to read from</param>
+        /// <returns>Texture</returns>
         public override Texture CreateTexture(Stream stream) {
             return new TextureGL(this, stream);
         }
-
+        /// <summary>
+        /// Creates a Empty Texture given a Size
+        /// </summary>
+        /// <param name="width">Width of Texture</param>
+        /// <param name="height">Height of Texture</param>
+        /// <returns>Texture</returns>
         public override Texture CreateTexture(uint width, uint height) {
             return new TextureGL(this, width, height);
         }
-
+        /// <summary>
+        /// Creates a Texture from a File
+        /// </summary>
+        /// <param name="filepath">Filepath to Image</param>
+        /// <returns>Texture</returns>
         public override Texture CreateTexture(string filepath) {
             return new TextureGL(this, filepath);
         }
-
+        /// <summary>
+        /// Used to Create a 1x1 Texture with only a white pixel
+        /// </summary>
+        /// <returns>White Pixel Texture</returns>
         public override Texture CreateWhitePixelTexture() {
             return new TextureGL(this);
         }
+        /// <summary>
+        /// Used to Update the ImGuiController in charge of rendering ImGui on this backend
+        /// </summary>
+        /// <param name="deltaTime">Delta Time</param>
         public override void ImGuiUpdate(double deltaTime) {
-            this._imGuiController.Update((float)deltaTime);
+            this.ImGuiController.Update((float)deltaTime);
         }
-        public override void ImGuiDraw(double deltatime) {
-            this._imGuiController.Render();
+        /// <summary>
+        /// Used to Draw the ImGuiController in charge of rendering ImGui on this backend
+        /// </summary>
+        /// <param name="deltaTime">Delta Time</param>
+        public override void ImGuiDraw(double deltaTime) {
+            this.ImGuiController.Render();
         }
-
+        /// <summary>
+        /// Returns the OpenGLES API
+        /// </summary>
+        /// <returns></returns>
         public GL GetGlApi() => this.gl;
-
         /// <summary>
         /// Debug Callback
         /// </summary>
