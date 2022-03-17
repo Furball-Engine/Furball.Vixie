102b42b3198cec073346caeedf5d14b1fb934d8c
Eevee <developerbrzoza@gmail.com>
2022-03-17 17:36:29 +0100

    Remove one of the TODOs from the list

--- Furball.Vixie.TestApplication/Program.cs
+++ Furball.Vixie.TestApplication/Program.cs
@@ -4,9 +4,7 @@ using Silk.NET.Windowing;
 
 var options = WindowOptions.Default;
 options.VSync           = false;
-//options.WindowBorder = WindowBorder.Fixed;
 
 new TestGame(options).Run(options, Backend.OpenGLES);
 
-//TODO: do the auto dispose thingy, the second this thing starts its gonna leak like fucking 50gb of memory within 5 seconds
 //TODO: also cleanup everything i made during crunch, this is probably some of the worst refactoring uve ever seen
