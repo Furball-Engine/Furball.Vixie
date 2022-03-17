5cebd8ba11d7cab91e0a8152759ba5b6e9f15339
Eevee <developerbrzoza@gmail.com>
2022-03-17 17:44:40 +0100

    Move TextureRenderTarget where it's supposed to be

--- Furball.Vixie/Graphics/TextureRenderTarget.cs
+++ Furball.Vixie/Graphics/TextureRenderTarget.cs
@@ -1,6 +1,7 @@
 using System.Numerics;
+using Furball.Vixie.Graphics.Backends;
 
-namespace Furball.Vixie.Graphics.Backends {
+namespace Furball.Vixie.Graphics {
     public abstract class TextureRenderTarget {
         public abstract Vector2 Size { get; protected set; }
 
