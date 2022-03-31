d2d3a7fe71920397aeca442d7da0b7834cfb1c3c
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-03-31 01:45:40 -0700

    Tests: Make Line Smiley test have variable line widths

--- Furball.Vixie.TestApplication/Tests/TestLineSmiley.cs
+++ Furball.Vixie.TestApplication/Tests/TestLineSmiley.cs
@@ -8,7 +8,9 @@ using ImGuiNET;
 namespace Furball.Vixie.TestApplication.Tests {
     public class TestLineSmiley : GameComponent {
         private ILineRenderer renderer;
-        
+
+        private float _lineWidth = 2f;
+
         public override void Initialize() {
             this.renderer = GraphicsBackend.Current.CreateLineRenderer();
             
@@ -20,9 +22,9 @@ namespace Furball.Vixie.TestApplication.Tests {
 
             this.renderer.Begin();
             
-            this.renderer.Draw(new(100, 100), new(100, 200), 2, Color.Blue);
-            this.renderer.Draw(new(200, 100), new(200, 200), 2, Color.Green);
-            this.renderer.Draw(new(100, 300), new(200, 300), 2, Color.Orange);
+            this.renderer.Draw(new(200, 200), new(200, 400), _lineWidth, Color.Blue);
+            this.renderer.Draw(new(400, 200), new(400, 400), _lineWidth, Color.Green);
+            this.renderer.Draw(new(200, 600), new(400, 600), _lineWidth, Color.Orange);
             
             this.renderer.End();
             
@@ -32,6 +34,8 @@ namespace Furball.Vixie.TestApplication.Tests {
                        $"Framerate: {Math.Round(ImGui.GetIO().Framerate,           2).ToString(CultureInfo.InvariantCulture)}"
             );
             
+            ImGui.SliderFloat("Line Width", ref this._lineWidth, 0f, 20f);
+
             if (ImGui.Button("Go back to test selector")) {
                 this.BaseGame.Components.Add(new BaseTestSelector());
                 this.BaseGame.Components.Remove(this);
