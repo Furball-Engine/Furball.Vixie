f689d909daaa9d9dce9d004c47a12af3830c4934
Furball <33634370+Eeveelution@users.noreply.github.com>
2022-07-31 16:51:39 +0200

    Direct3D9: Create project file

---
+++ Furball.Vixie.Backends.Direct3D9/Direct3D9Backend.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using Furball.Vixie.Backends.Shared;
+using Furball.Vixie.Backends.Shared.Backends;
+using Furball.Vixie.Backends.Shared.Renderers;
+using Silk.NET.Input;
+using Silk.NET.Windowing;
+using SixLabors.ImageSharp;
+
+namespace Furball.Vixie.Backends.Direct3D9 {
+    public class Direct3D9Backend : IGraphicsBackend {
+        public override void Initialize(IView view, IInputContext inputContext) {
+
+        }
+
+        public override void Cleanup() {
+            throw new NotImplementedException();
+        }
+
+        public override void HandleFramebufferResize(int width, int height) {
+            throw new NotImplementedException();
+        }
+
+        public override IQuadRenderer CreateTextureRenderer() => throw new NotImplementedException();
+
+        public override ILineRenderer CreateLineRenderer() => throw new NotImplementedException();
+
+        public override int QueryMaxTextureUnits() => throw new NotImplementedException();
+
+        public override void Clear() {
+            throw new NotImplementedException();
+        }
+
+        public override void TakeScreenshot() {
+            throw new NotImplementedException();
+        }
+
+        public override Rectangle ScissorRect { get; set; }
+
+        public override void SetFullScissorRect() {
+            throw new NotImplementedException();
+        }
+
+        public override TextureRenderTarget CreateRenderTarget(uint width, uint height) => throw new NotImplementedException();
+
+        public override Texture CreateTexture(byte[] imageData, bool qoi = false) => throw new NotImplementedException();
+
+        public override Texture CreateTexture(Stream stream) => throw new NotImplementedException();
+
+        public override Texture CreateTexture(uint width, uint height) => throw new NotImplementedException();
+
+        public override Texture CreateTexture(string filepath) => throw new NotImplementedException();
+
+        public override Texture CreateWhitePixelTexture() => throw new NotImplementedException();
+
+        public override void ImGuiUpdate(double deltaTime) {
+            throw new NotImplementedException();
+        }
+
+        public override void ImGuiDraw(double deltaTime) {
+            throw new NotImplementedException();
+        }
+    }
+}
