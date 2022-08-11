312e0d9fb3b7183a9afc0c249d5ceea89bd8e32e
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-08-11 13:34:46 -0700

    Finish rebasing

--- Furball.Vixie.Backends.Direct3D9/Direct3D9Backend.cs
+++ Furball.Vixie.Backends.Direct3D9/Direct3D9Backend.cs
@@ -12,175 +12,178 @@ using Silk.NET.Windowing;
 using Rectangle=SixLabors.ImageSharp.Rectangle;
 using Texture=Furball.Vixie.Backends.Shared.Texture;
 
-namespace Furball.Vixie.Backends.Direct3D9 {
-    public class Direct3D9Backend : IGraphicsBackend {
-        private Direct3D      _direct3D;
-        private Device        _device;
-        private SwapChain     _swapChain;
-        private RawColorBGRA  _clearColor = new RawColorBGRA(0, 0, 0, 255);
-        private Vector2D<int> _currentViewport;
+namespace Furball.Vixie.Backends.Direct3D9; 
 
-        public static int DeviceOverride = 0;
+public class Direct3D9Backend : IGraphicsBackend {
+    private Direct3D      _direct3D;
+    private Device        _device;
+    private SwapChain     _swapChain;
+    private RawColorBGRA  _clearColor = new RawColorBGRA(0, 0, 0, 255);
+    private Vector2D<int> _currentViewport;
 
-        private bool TryCreateDevice(DeviceType type, int deviceId, IntPtr hwnd, PresentParameters presentParameters, out Device device) {
-            Capabilities caps = this._direct3D.GetDeviceCaps(deviceId, type);
+    public static int DeviceOverride = 0;
 
-            Logger.Log($"Trying to create Device [{deviceId}] as {type.ToString()}", LoggerLevelD3D9.InstanceInfo);
+    private bool TryCreateDevice(DeviceType type, int deviceId, IntPtr hwnd, PresentParameters presentParameters, out Device device) {
+        Capabilities caps = this._direct3D.GetDeviceCaps(deviceId, type);
 
-            //Check for all the device features we require
-            if ((caps.TextureCaps & TextureCaps.Pow2)               != 0 ||
-                (caps.TextureCaps & TextureCaps.NonPow2Conditional) != 0 ||
-                (caps.TextureCaps & TextureCaps.SquareOnly)         != 0 ||
-                (caps.VertexShaderVersion.Major < 2)                     ||
-                (caps.PixelShaderVersion.Major  < 2)
-            ) {
-                device = null;
+        Logger.Log($"Trying to create Device [{deviceId}] as {type.ToString()}", LoggerLevelD3D9.InstanceInfo);
 
-                Logger.Log($"Creating Device [{deviceId}] as {type.ToString()} failed!", LoggerLevelD3D9.InstanceError);
-                Logger.Log($"The device either doesn't support NPOT at all times, or non-square textures, or a Vertex or Pixel shader version of at least 2.0", LoggerLevelD3D9.InstanceError);
+        //Check for all the device features we require
+        if ((caps.TextureCaps & TextureCaps.Pow2)               != 0 ||
+            (caps.TextureCaps & TextureCaps.NonPow2Conditional) != 0 ||
+            (caps.TextureCaps & TextureCaps.SquareOnly)         != 0 ||
+            (caps.VertexShaderVersion.Major < 2)                     ||
+            (caps.PixelShaderVersion.Major  < 2)
+           ) {
+            device = null;
 
-                return false;
-            }
+            Logger.Log($"Creating Device [{deviceId}] as {type.ToString()} failed!", LoggerLevelD3D9.InstanceError);
+            Logger.Log($"The device either doesn't support NPOT at all times, or non-square textures, or a Vertex or Pixel shader version of at least 2.0", LoggerLevelD3D9.InstanceError);
 
-            CreateFlags createFlags = (caps.DeviceCaps & DeviceCaps.HWTransformAndLight) != 0
-                                          ? CreateFlags.HardwareVertexProcessing
-                                          : CreateFlags.SoftwareVertexProcessing;
-
-
-            device = new Device(this._direct3D, deviceId, type, hwnd, createFlags, presentParameters);
-
-            if (device == null)
-                return false;
-
-            return true;
+            return false;
         }
 
-        public override void Initialize(IView view, IInputContext inputContext) {
-            this._direct3D = new Direct3D();
+        CreateFlags createFlags = (caps.DeviceCaps & DeviceCaps.HWTransformAndLight) != 0
+            ? CreateFlags.HardwareVertexProcessing
+            : CreateFlags.SoftwareVertexProcessing;
 
-            PresentParameters presentParameters = new PresentParameters {
-                BackBufferCount = 1,
-                BackBufferWidth = view.FramebufferSize.X,
-                BackBufferHeight = view.FramebufferSize.Y,
-                BackBufferFormat = Format.X8R8G8B8,
-                Windowed = true,
-                SwapEffect = SwapEffect.Copy
-            };
 
-            for (int i = 0; i != this._direct3D.AdapterCount; i++) {
-                AdapterDetails details = this._direct3D.GetAdapterIdentifier(i);
+        device = new Device(this._direct3D, deviceId, type, hwnd, createFlags, presentParameters);
 
-                BackendInfoSection deviceInformation = new BackendInfoSection($"Device #{i}");
+        if (device == null)
+            return false;
 
-                deviceInformation.Contents.Add( ("Driver", details.Driver) );
-                deviceInformation.Contents.Add( ("Adapter Description", details.Description) );
-                deviceInformation.Contents.Add( ("Device Name", details.DeviceName) );
-                deviceInformation.Contents.Add( ("Driver Version", details.DriverVersion.ToString()) );
+        return true;
+    }
 
-                this.InfoSections.Add(deviceInformation);
-            }
+    public override void Initialize(IView view, IInputContext inputContext) {
+        this._direct3D = new Direct3D();
 
-            this.InfoSections.ForEach(x => x.Log(LoggerLevelD3D9.InstanceInfo));
+        PresentParameters presentParameters = new PresentParameters {
+            BackBufferCount  = 1,
+            BackBufferWidth  = view.FramebufferSize.X,
+            BackBufferHeight = view.FramebufferSize.Y,
+            BackBufferFormat = Format.X8R8G8B8,
+            Windowed         = true,
+            SwapEffect       = SwapEffect.Copy
+        };
 
-            if (DeviceOverride != 0) {
-                if( TryCreateDevice(DeviceType.Hardware, DeviceOverride, view.Native.Win32.Value.Hwnd, presentParameters, out this._device) )
-                    goto DeviceCreated;
-                if( TryCreateDevice(DeviceType.Hardware, DeviceOverride, view.Native.Win32.Value.Hwnd, presentParameters, out this._device) )
-                    goto DeviceCreated;
-            }
+        for (int i = 0; i != this._direct3D.AdapterCount; i++) {
+            AdapterDetails details = this._direct3D.GetAdapterIdentifier(i);
 
-            for (int i = 0; i != this._direct3D.AdapterCount; i++) {
-                if( TryCreateDevice(DeviceType.Hardware, i, view.Native.Win32.Value.Hwnd, presentParameters, out this._device) )
-                    goto DeviceCreated;
-                if( TryCreateDevice(DeviceType.Hardware, i, view.Native.Win32.Value.Hwnd, presentParameters, out this._device) )
-                    goto DeviceCreated;
-            }
+            BackendInfoSection deviceInformation = new BackendInfoSection($"Device #{i}");
 
-            DeviceCreated: ;
+            deviceInformation.Contents.Add( ("Driver", details.Driver) );
+            deviceInformation.Contents.Add( ("Adapter Description", details.Description) );
+            deviceInformation.Contents.Add( ("Device Name", details.DeviceName) );
+            deviceInformation.Contents.Add( ("Driver Version", details.DriverVersion.ToString()) );
 
-            if (this._device == null)
-                throw new Exception("No suitable Direct3D9 Device found which matches Vixie's requirements!");
-
-            this._currentViewport = new Vector2D<int>(view.FramebufferSize.X, view.FramebufferSize.Y);
+            this.InfoSections.Add(deviceInformation);
         }
 
-        public override void Cleanup() {
-            this._device.Dispose();
-            this._direct3D.Dispose();
+        this.InfoSections.ForEach(x => x.Log(LoggerLevelD3D9.InstanceInfo));
+
+        if (DeviceOverride != 0) {
+            if( TryCreateDevice(DeviceType.Hardware, DeviceOverride, view.Native.Win32.Value.Hwnd, presentParameters, out this._device) )
+                goto DeviceCreated;
+            if( TryCreateDevice(DeviceType.Hardware, DeviceOverride, view.Native.Win32.Value.Hwnd, presentParameters, out this._device) )
+                goto DeviceCreated;
         }
 
-        public override void HandleFramebufferResize(int width, int height) {
-            PresentParameters presentParameters = new PresentParameters {
-                BackBufferCount  = 1,
-                BackBufferWidth  = width,
-                BackBufferHeight = height,
-                BackBufferFormat = Format.X8R8G8B8,
-                Windowed         = true,
-                SwapEffect       = SwapEffect.Copy
-            };
-
-            this._device.Reset(presentParameters);
-
-            this._currentViewport = new Vector2D<int>(width, height);
+        for (int i = 0; i != this._direct3D.AdapterCount; i++) {
+            if( TryCreateDevice(DeviceType.Hardware, i, view.Native.Win32.Value.Hwnd, presentParameters, out this._device) )
+                goto DeviceCreated;
+            if( TryCreateDevice(DeviceType.Hardware, i, view.Native.Win32.Value.Hwnd, presentParameters, out this._device) )
+                goto DeviceCreated;
         }
 
-        public override IQuadRenderer CreateTextureRenderer() => throw new NotImplementedException();
+    DeviceCreated: ;
 
-        public override int QueryMaxTextureUnits() => throw new NotImplementedException();
+        if (this._device == null)
+            throw new Exception("No suitable Direct3D9 Device found which matches Vixie's requirements!");
 
-        public override void Clear() {
-            this._device.Clear(ClearFlags.Target, _clearColor, 0, 0);
+        this._currentViewport = new Vector2D<int>(view.FramebufferSize.X, view.FramebufferSize.Y);
+    }
+
+    public override void Cleanup() {
+        this._device.Dispose();
+        this._direct3D.Dispose();
+    }
+
+    public override void HandleFramebufferResize(int width, int height) {
+        PresentParameters presentParameters = new PresentParameters {
+            BackBufferCount  = 1,
+            BackBufferWidth  = width,
+            BackBufferHeight = height,
+            BackBufferFormat = Format.X8R8G8B8,
+            Windowed         = true,
+            SwapEffect       = SwapEffect.Copy
+        };
+
+        this._device.Reset(presentParameters);
+
+        this._currentViewport = new Vector2D<int>(width, height);
+    }
+
+    public override IQuadRenderer CreateTextureRenderer() => throw new NotImplementedException();
+
+    public override int QueryMaxTextureUnits() => throw new NotImplementedException();
+
+    public override void Clear() {
+        this._device.Clear(ClearFlags.Target, _clearColor, 0, 0);
+    }
+
+    public override void TakeScreenshot() {
+        throw new NotImplementedException();
+    }
+
+    private Rectangle _lastScissor;
+
+    public override Rectangle ScissorRect {
+        get => this._lastScissor;
+        set {
+            this._lastScissor = value;
+
+            this._device.ScissorRect = new RawRectangle(value.Left, value.Top, value.Right, value.Bottom);
         }
+    }
 
-        public override void TakeScreenshot() {
-            throw new NotImplementedException();
-        }
+    public override void SetFullScissorRect() {
+        this._device.ScissorRect = new RawRectangle(0, 0, this._currentViewport.X, this._currentViewport.Y);
+    }
 
-        private Rectangle _lastScissor;
+    public override TextureRenderTarget CreateRenderTarget(uint width, uint height)
+        => throw new NotImplementedException();
+        
+    public override Texture CreateTextureFromByteArray(byte[] imageData, TextureParameters parameters = default) {
+        throw new NotImplementedException();
+    }
+    public override Texture CreateTextureFromStream(Stream stream, TextureParameters parameters = default) {
+        throw new NotImplementedException();
+    }
+    public override Texture CreateEmptyTexture(uint width, uint height, TextureParameters parameters = default) {
+        throw new NotImplementedException();
+    }
 
-        public override Rectangle ScissorRect {
-            get => this._lastScissor;
-            set {
-                this._lastScissor = value;
+    public override Texture CreateWhitePixelTexture() => throw new NotImplementedException();
 
-                this._device.ScissorRect = new RawRectangle(value.Left, value.Top, value.Right, value.Bottom);
-            }
-        }
+    public override void ImGuiUpdate(double deltaTime) {
+        throw new NotImplementedException();
+    }
 
-        public override void SetFullScissorRect() {
-            this._device.ScissorRect = new RawRectangle(0, 0, this._currentViewport.X, this._currentViewport.Y);
-        }
+    public override void ImGuiDraw(double deltaTime) {
+        throw new NotImplementedException();
+    }
 
-        public override TextureRenderTarget CreateRenderTarget(uint width, uint height) => throw new NotImplementedException();
+    public override void BeginScene() {
+        this._device.BeginScene();
+    }
 
-        public override Texture CreateTexture(byte[] imageData, bool qoi = false) => throw new NotImplementedException();
+    public override void EndScene() {
+        this._device.EndScene();
+    }
 
-        public override Texture CreateTexture(Stream stream) => throw new NotImplementedException();
-
-        public override Texture CreateTexture(uint width, uint height) => throw new NotImplementedException();
-
-        public override Texture CreateTexture(string filepath) => throw new NotImplementedException();
-
-        public override Texture CreateWhitePixelTexture() => throw new NotImplementedException();
-
-        public override void ImGuiUpdate(double deltaTime) {
-            throw new NotImplementedException();
-        }
-
-        public override void ImGuiDraw(double deltaTime) {
-            throw new NotImplementedException();
-        }
-
-        public override void BeginScene() {
-            this._device.BeginScene();
-        }
-
-        public override void EndScene() {
-            this._device.EndScene();
-        }
-
-        public override void Present() {
-            this._device.Present();
-        }
+    public override void Present() {
+        this._device.Present();
     }
 }
