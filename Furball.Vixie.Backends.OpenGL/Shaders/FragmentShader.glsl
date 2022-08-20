fe02015388802607161eb96195b138bcac9a5157
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-08-20 09:38:33 -0700

    OpenGL: Remove in/out layout qualifiers

--- Furball.Vixie.Backends.OpenGL/Shaders/FragmentShader.glsl
+++ Furball.Vixie.Backends.OpenGL/Shaders/FragmentShader.glsl
@@ -1,9 +1,9 @@
 #version 330
 precision mediump float;
 
-layout(location = 0) in vec4 _Color;
-layout(location = 1) in vec2 _TextureCoordinate;
-layout(location = 2) flat in int _TextureId;
+in vec4 _Color;
+in vec2 _TextureCoordinate;
+flat in int _TextureId;
 
 out vec4 OutputColor;
 
