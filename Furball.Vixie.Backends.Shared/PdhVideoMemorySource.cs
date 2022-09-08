1efa8616ea9e20ad803541d6e19db654fd69a1be
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-09-07 22:12:15 -0700

    Begin implementing Win32 VRAM perf grabber

---
+++ Furball.Vixie.Backends.Shared/PdhVideoMemorySource.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Vanara.PInvoke;
+
+namespace Furball.Vixie.Backends.Shared;
+
+public class PdhVideoMemorySource : VideoMemorySource {
+    public PdhVideoMemorySource() {
+        //Connect to the local machine
+        Pdh.PdhConnectMachine();
+
+        //Open a query
+        Win32Error result = Pdh.PdhOpenQuery(null, IntPtr.Zero, out Pdh.SafePDH_HQUERY query);
+        if (result != Win32Error.ERROR_SUCCESS)
+            throw new Exception("Failed to open PDH query!");
+    }
+
+    public override ulong TotalVideoMemory() => throw new NotImplementedException();
+    public override ulong UsedVideoMemory()  => throw new NotImplementedException();
+}
---
+++ Furball.Vixie.Backends.Shared/VideoRamSource.cs
@@ -0,0 +1,6 @@
+﻿namespace Furball.Vixie.Backends.Shared;
+
+public abstract class VideoMemorySource {
+    public abstract ulong TotalVideoMemory();
+    public abstract ulong UsedVideoMemory();
+}
