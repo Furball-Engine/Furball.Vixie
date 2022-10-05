729b8c1359f926d3884e61112f62b9664d7e5b76
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-10-04 19:32:54 -0700

    NuGet: Update dependencies and remove VideoRamSource

--- Furball.Vixie.Backends.Shared/VideoRamSource.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Furball.Vixie.Backends.Shared;
-
-public abstract class VideoMemorySource {
-    public abstract ulong TotalVideoMemory();
-    public abstract ulong UsedVideoMemory();
-}
