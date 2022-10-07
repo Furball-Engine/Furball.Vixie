901fa1ee65c3dd0be14605697b6bfb17848db112
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-10-07 16:45:13 -0700

    OpenGL: Start to the FFP implementation

--- Furball.Vixie.Backends.OpenGL/FixedFunctionOpenGLRenderer.cs
+++ Furball.Vixie.Backends.OpenGL/FixedFunctionOpenGLRenderer.cs
@@ -1,16 +1,55 @@
 ﻿using System;
+using System.Numerics;
+using System.Runtime.InteropServices;
 using Furball.Vixie.Backends.Shared;
 using Furball.Vixie.Backends.Shared.Renderers;
 using Furball.Vixie.Helpers;
+using Silk.NET.OpenGL.Legacy;
+using SixLabors.ImageSharp.PixelFormats;
 
-namespace Furball.Vixie.Backends.OpenGL; 
+//Disable obsolete warning :^)
+#pragma warning disable CS0618
+
+namespace Furball.Vixie.Backends.OpenGL;
 
 public class FixedFunctionOpenGLRenderer : Renderer {
+    private readonly OpenGLBackend _backend;
+    private readonly GL            _gl;
+
+    private uint _list;
+
+    public FixedFunctionOpenGLRenderer(OpenGLBackend backend) {
+        this._backend = backend;
+
+        this._gl = backend.GetLegacyGl();
+
+        unsafe {
+            uint stride = (uint)sizeof(Vertex);
+
+            this._gl.VertexPointer(2, VertexPointerType.Float, stride,
+                (void*)0);
+            this._gl.ColorPointer(4, ColorPointerType.UnsignedByte, stride,
+                (void*)sizeof(Vector2));
+            this._gl.TexCoordPointer(2, TexCoordPointerType.Float, stride, (void*)(sizeof(Vector2) + sizeof(Rgba32)));
+        }
+        
+        this._list = this._gl.GenLists(1);
+    }
+
     public override void Begin() {
         Guard.Todo("Implement FFP Begin");
+
     }
     public override void End() {
         Guard.Todo("Implement FFP End");
+
+        //Start a new display list
+        this._gl.NewList(this._list, ListMode.Compile);
+
+        //TODO: Convert all the commands to OpenGL commands
+
+        //End the display list
+        this._gl.EndList();
     }
     public override MappedData Reserve(ushort vertexCount, uint indexCount) {
         Guard.Todo("Implement FFP Reserve");
@@ -24,8 +63,10 @@ public class FixedFunctionOpenGLRenderer : Renderer {
     }
     public override void Draw() {
         Guard.Todo("Implement FFP Draw");
+
+
     }
     protected override void DisposeInternal() {
-        
+        this._gl.DeleteLists(this._list, 1);
     }
 }
