fe8b3e9715ef86f9feb1cfbd4bfd25b9a83ba926
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-12-20 11:00:15 -0800

    ImGui: Dont use GCHandle

--- Furball.Vixie.Backends.Shared/ImGuiController/ImGuiControllerShared.cs
+++ Furball.Vixie.Backends.Shared/ImGuiController/ImGuiControllerShared.cs
@@ -16,8 +16,8 @@ namespace Furball.Vixie.Backends.Shared.ImGuiController;
 public sealed unsafe class ImGuiControllerShared : ImGuiController {
     private readonly GraphicsBackend _backend;
     private          VixieRenderer   _renderer = null!;
-    private          Rectangle       _scissor;
-    private          GCHandle        _fontTexture;
+    private          VixieTexture?   _fontTexture;
+    // private          Rectangle       _scissor;
 
     public ImGuiControllerShared(GraphicsBackend backend, IView view, IInputContext input, ImGuiFontConfig?
                                      imGuiFontConfig = null,
@@ -36,12 +36,12 @@ public sealed unsafe class ImGuiControllerShared : ImGuiController {
     }
 
     protected override void Draw(ImDrawDataPtr drawDataPtr) {
-        int framebufferWidth  = (int)(drawDataPtr.DisplaySize.X * drawDataPtr.FramebufferScale.X);
-        int framebufferHeight = (int)(drawDataPtr.DisplaySize.Y * drawDataPtr.FramebufferScale.Y);
+        // int framebufferWidth  = (int)(drawDataPtr.DisplaySize.X * drawDataPtr.FramebufferScale.X);
+        // int framebufferHeight = (int)(drawDataPtr.DisplaySize.Y * drawDataPtr.FramebufferScale.Y);
 
         // Will project scissor/clipping rectangles into framebuffer space
-        Vector2 clipOff   = drawDataPtr.DisplayPos;       // (0,0) unless using multi-viewports
-        Vector2 clipScale = drawDataPtr.FramebufferScale; // (1,1) unless using retina display which are often (2,2)
+        // Vector2 clipOff   = drawDataPtr.DisplayPos;       // (0,0) unless using multi-viewports
+        // Vector2 clipScale = drawDataPtr.FramebufferScale; // (1,1) unless using retina display which are often (2,2)
 
         this._renderer.Begin(CullFace.None);
 
@@ -53,14 +53,9 @@ public sealed unsafe class ImGuiControllerShared : ImGuiController {
 
             if (cmdListPtr.CmdBuffer.Size == 0)
                 break;
-            
-            GCHandle handle = GCHandle.FromIntPtr(cmdListPtr.CmdBuffer[0].TextureId);
-                    
-            if(!handle.IsAllocated || handle.Target is not VixieTexture tex)
-                throw new Exception("Invalid texture in ImGui!");
 
             MappedData map =
-                this._renderer.Reserve((ushort)cmdListPtr.VtxBuffer.Size, (uint)cmdListPtr.IdxBuffer.Size, tex);
+                this._renderer.Reserve((ushort)cmdListPtr.VtxBuffer.Size, (uint)cmdListPtr.IdxBuffer.Size, this._fontTexture!);
             new Span<ushort>((void*)cmdListPtr.IdxBuffer.Data, cmdListPtr.IdxBuffer.Size).CopyTo(new Span<ushort>(map.IndexPtr, cmdListPtr.IdxBuffer.Size));
             for (int i = 0; i < cmdListPtr.IdxBuffer.Size; i++) {
                 map.IndexPtr[i] += (ushort)map.IndexOffset;
@@ -85,7 +80,10 @@ public sealed unsafe class ImGuiControllerShared : ImGuiController {
     protected override void CreateDeviceResources() {
         this.RecreateFontDeviceTexture();
     }
+    
     protected override void RecreateFontDeviceTexture() {
+        this._fontTexture?.Dispose();
+        
         // Build texture atlas
         // Load as RGBA 32-bit (75% of the memory is wasted, but default font is so small)
         // because it is more likely to be compatible with user's existing shaders.
@@ -100,13 +98,13 @@ public sealed unsafe class ImGuiControllerShared : ImGuiController {
 
         tex.SetData(new ReadOnlySpan<Rgba32>((void*)pixels, width * height * sizeof(Rgba32)));
 
-        GCHandle handle = this._fontTexture = GCHandle.Alloc(tex);
+        this._fontTexture = tex;
         
-        io.Fonts.SetTexID(GCHandle.ToIntPtr(handle));
+        io.Fonts.SetTexID(IntPtr.Zero);
     }
 
     protected override void DisposeInternal() {
         this._renderer.Dispose();
-        this._fontTexture.Free();
+        this._fontTexture?.Dispose();
     }
 }
